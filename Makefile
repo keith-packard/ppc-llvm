0daa92b95c70425226c86af1807730fc0adc429d
Keith Packard <keithp@keithp.com>
2024-10-06 17:40:11 -0700

    Shrink the example to a single line of code

--- Makefile
+++ Makefile
@@ -5,7 +5,7 @@ CFLAGS= \
 	-fstack-protector-strong \
 	-mstack-protector-guard=tls \
 	-mstack-protector-guard-reg=r13 \
-	-mstack-protector-guard-offset=3192
+	-mstack-protector-guard-offset=0
 
 foo.s: foo.c Makefile
 	$(CC) $(CFLAGS) -S foo.c
--- foo.c
+++ foo.c
@@ -4,35 +4,13 @@
  *
  *  Copyright (C) 1991, 1992  Linus Torvalds
  */
-typedef unsigned long size_t;
+void _parse_integer_fixup_radix(const char *s, unsigned int *base);
 
-const char *_parse_integer_fixup_radix(const char *s, unsigned int *base);
-unsigned int _parse_integer_limit(const char *s, unsigned int base, unsigned long long *res,
-      size_t max_chars);
-unsigned int _parse_integer(const char *s, unsigned int base, unsigned long long *res);
+unsigned long long simple_strntoull(const char *startp);
 
-unsigned long long simple_strntoull(const char *startp, char **endp, unsigned int base, size_t max_chars);
-
-unsigned long long simple_strntoull(const char *startp, char **endp, unsigned int base, size_t max_chars)
+unsigned long long simple_strntoull(const char *startp)
 {
- const char *cp;
- unsigned long long result = 0ULL;
- size_t prefix_chars;
- unsigned int rv;
-
- cp = _parse_integer_fixup_radix(startp, &base);
- prefix_chars = cp - startp;
- if (prefix_chars < max_chars) {
-  rv = _parse_integer_limit(cp, base, &result, max_chars - prefix_chars);
-
-  cp += (rv & ~(1U << 31));
- } else {
-
-  cp = startp + max_chars;
- }
-
- if (endp)
-  *endp = (char *)cp;
-
- return result;
+	unsigned int base;
+	_parse_integer_fixup_radix(startp, &base);
+	return 0ULL;
 }
